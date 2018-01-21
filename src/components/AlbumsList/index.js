d925d517d5d3e95da3abcfe273654b2b12d146ba
Andrew <vray1995@gmail.com>
2018-01-21 22:52:10 +0300

    edit AlbumsList component
    1. install react-slick
    2. edit AlbumsList component
    3. add css for AlbumsList component

--- src/components/AlbumsList/index.js
+++ src/components/AlbumsList/index.js
@@ -2,23 +2,38 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Card, Image } from 'semantic-ui-react';
 import defaultAlbum from '../../static/defaultAlbum.png';
+import Slider from 'react-slick';
+const style = require('./AlbumList.sass');
 
 class AlbumsList extends Component {
   render() {
     const { items } = this.props;
-    return (<Card.Group itemsPerRow={4} stackable>
-      {
-        items.length ? items.map((item) => {
-          return <Card key={item.id}
-                       color='red'
-                       as='a'
-                       href={'/album/' + item.id}>
-            <Image src={defaultAlbum}/>
-            <Card.Header textAlign='center'>{item.title}</Card.Header>
-          </Card>
-        }) : null
-      }
-    </Card.Group>);
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 1
+    };
+    return (<div className={style.AlbumsList}>
+      <h2 className="BlockTitle">User's albums</h2>
+      <div>
+        <Slider {...settings}>
+          {
+            items.length ? items.map((item) => {
+              return <Card key={item.id}
+                           color='red'
+                           as='a'
+                           href={'/album/' + item.id}>
+                <Image src={defaultAlbum}/>
+                <Card.Header textAlign='center'>{item.title}</Card.Header>
+              </Card>
+            }) : null
+          }
+        </Slider>
+
+      </div>
+    </div>);
   }
 }
 
