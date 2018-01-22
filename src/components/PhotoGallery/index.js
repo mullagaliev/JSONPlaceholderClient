8c081ebb5eb65cd6354a3ec9ad921ab6f5911172
Andrew <vray1995@gmail.com>
2018-01-22 16:59:26 +0300

    Add Lightbox to PhotoGallery component
    1. Install package react-image-lightbox
    2. Add lightbox to PhotoGallery
    3. Add different cursor styles for PhotoGalleryElemLink and PhotoGalleryElem

--- src/components/PhotoGallery/index.js
+++ src/components/PhotoGallery/index.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import PhotoGalleryElem from './PhotoGalleryElem';
+import Lightbox from 'react-image-lightbox';
 
 const style = require('./PhotoGallery.sass');
 
 class PhotoGallery extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      photoIndex: 0,
+      isOpen: false,
+    };
+  }
+
   render() {
+    const { photoIndex, isOpen } = this.state;
     const { items, elemLinkPattern } = this.props;
+    const images = items.map((item) => {
+      return item.url;
+    });
+    const titles = items.map((item) => {
+      return item.title;
+    });
+    console.log(titles);
     if (!Boolean(items.length))
       return null;
     return (
         <div className={style.PhotoGallery}>
           {
-            items.map((item) => {
+            items.map((item, id) => {
               if (Boolean(elemLinkPattern)) {
                 return <PhotoGalleryElem key={item.id}
                                          as='a'
@@ -20,8 +38,33 @@ class PhotoGallery extends Component {
                                          item={item}/>;
               }
               return <PhotoGalleryElem key={item.id}
+                                       onClick={() => this.setState({
+                                         isOpen: true,
+                                         photoIndex: id
+                                       })}
                                        item={item}/>;
             })
+          }
+          {
+            isOpen && (
+                <Lightbox
+                    imageTitle={titles[photoIndex]}
+                    mainSrc={images[photoIndex]}
+                    nextSrc={images[(photoIndex + 1) % images.length]}
+                    prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+                    onCloseRequest={() => this.setState({ isOpen: false })}
+                    onMovePrevRequest={() =>
+                        this.setState({
+                          photoIndex: (photoIndex + images.length - 1) % images.length,
+                        })
+                    }
+                    onMoveNextRequest={() =>
+                        this.setState({
+                          photoIndex: (photoIndex + 1) % images.length,
+                        })
+                    }
+                />
+            )
           }
         </div>
     );
